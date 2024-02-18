9b93883b768e92e6934b677b6a5c70b1dcb290f0
Xfarkasp <farkas@smartvision.eu>
2024-02-18 14:47:36 +0100

    prototype

---
+++ Switch.py
@@ -0,0 +1,133 @@
+from scapy.all import sniff, ARP, Ether
+import psutil
+import threading
+import time
+
+from PyQt5.QtCore import QObject, pyqtSignal
+
+BC_MAC = "FF:FF:FF:FF:FF:FF"
+class Switch(QObject):
+    # signals
+    port0_changed = pyqtSignal(str)
+    port1_changed = pyqtSignal(str)
+    log_value_changed = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()  # Call the superclass __init__ method
+        self._port0_address = ""
+        self._port1_address = ""
+        self._log_value = ""
+
+        self.connected_devices = {}  # Dictionary to store connected devices and their sockets
+        self.mac_to_port = {'0': 'NONE', '1': 'NONE'}
+        self.packet_timeout = 30  # Timeout for MAC address entries in seconds
+
+        self.log_value = f"a"
+
+
+    def start_sniffing(self):
+        sniff(prn=self.packet_callback, store=0)
+
+
+    @property
+    def port0_address(self):
+        return self._port0_address
+
+    @property
+    def port1_address(self):
+        return self._port1_address
+
+    @property
+    def log_value(self):
+        return self._log_value
+
+    @port0_address.setter
+    def port0_address(self, new_value):
+        self._port0_address = new_value
+        self.port0_changed.emit(new_value)
+
+    @port1_address.setter
+    def port1_address(self, new_value):
+        self._port1_address = new_value
+        self.port1_changed.emit(new_value)
+
+    @log_value.setter
+    def log_value(self, new_value):
+        self._log_value = new_value
+        print("aaaaaaaaaa")
+        self.log_value_changed.emit(new_value)
+
+    def determine_port(self, src_mac):
+        if src_mac not in self.mac_to_port:
+            # Assign a new port for the unknown MAC address
+            port = len(self.mac_to_port) + 1
+            self.mac_to_port[src_mac] = port
+        return self.mac_to_port[src_mac]
+
+    def get_interface_name(self, packet):
+        for interface, addrs in psutil.net_if_addrs().items():
+            for addr in addrs:
+                if addr.address == packet[Ether].src:
+                    print(interface)
+                    return interface
+        return "Unknown"
+
+    def packet_callback(self, packet):
+        if packet.haslayer("Ethernet"):
+            src_mac = packet["Ethernet"].src
+            dst_mac = packet["Ethernet"].dst
+            type = packet["Ethernet"].fields["type"]
+
+            print(f"Received frame from {src_mac} to {dst_mac} type {type}")
+            self.log_value = f"Received frame from {src_mac} to {dst_mac} type {type}"
+            interface = self.get_interface_name(packet)
+            print(f"Frame received on interface: {interface}")
+            # Access packet information as needed
+            print(packet.summary())
+
+
+    def add_device(self, device_address, device_socket):
+        self.connected_devices[device_address] = device_socket
+        # Associate the MAC address with a switch port (assuming only two ports)
+        if len(self.mac_to_port) < 2:
+            self.mac_to_port[device_address] = len(self.mac_to_port)
+
+
+    def remove_device(self, device_address):
+        del self.connected_devices[device_address]
+        if device_address in self.mac_to_port:
+            del self.mac_to_port[device_address]
+
+
+    def forward_packet(self, source_address, destination_address, packet):
+        destination_device = self.connected_devices.get(destination_address)
+       # if destination_address == BC_MAC:
+
+        if destination_device:
+            print(f"Switch: Forwarding packet from {source_address} to {destination_address}")
+            destination_device.sendall(packet)
+            # Update the timestamp for the MAC address associated with the outgoing port
+            self.mac_to_port[source_address] = time.time()
+
+
+    def check_timeout(self):
+        while True:
+            time.sleep(1)
+            current_time = time.time()
+            # Check for MAC addresses that have exceeded the timeout
+            for mac_address, timestamp in list(self.mac_to_port.items()):
+                if current_time - timestamp > self.packet_timeout:
+                    print(f"Timeout for MAC address {mac_address}. Removing association.")
+                    del self.mac_to_port[mac_address]
+
+def send_synthetic_packet(interface):
+    available_interfaces = psutil.net_if_addrs().keys()
+    print(f"Available interfaces: {', '.join(available_interfaces)}")
+
+    # Check if the specified interface exists and is not a loopback interface
+    if interface in available_interfaces and not psutil.net_if_stats()[interface].isup:
+        mac_address = psutil.net_if_addrs()[interface][0].address
+        # Rest of your code for sending synthetic packet...
+        print(f"Sending synthetic packet on {interface} with MAC address: {mac_address}")
+    else:
+        print(f"Interface {interface} is not available or is a loopback interface.")
---
+++ gui_window.py
@@ -0,0 +1,91 @@
+import sys
+import threading
+
+from PyQt5.QtCore import QTimer, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, \
+    QPlainTextEdit, QSizePolicy, QLabel
+
+from Switch import Switch, send_synthetic_packet
+
+
+class TableExample(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.output_text = QPlainTextEdit(self)
+
+        # Create an instance of the logic class
+        self.switch = Switch()
+
+        # Connect the custom signal to a slot (method) in the GUI class
+        self.switch.log_value_changed.connect(self.add_text)
+
+        self.initUI()
+
+        # self.switch.log_value = "asdasd"
+
+        self.add_text("aaa")
+
+        # Start a separate thread for sniffing packets
+        sniff_thread = threading.Thread(target=self.switch.start_sniffing)
+        sniff_thread.start()
+
+        send_synthetic_packet("wi-fi")
+
+    @pyqtSlot(str)
+    def add_text(self, text):
+        current_text = self.output_text.toPlainText()
+        new_text = f'{text}\n'
+        self.output_text.setPlainText(current_text + new_text)
+
+    def initUI(self):
+        table = QTableWidget(self)
+        table.setRowCount(2)
+        table.setColumnCount(2)
+
+        # Set the table headers
+        table.setVerticalHeaderLabels(['PORT 0', 'PORT 1'])
+        table.setHorizontalHeaderLabels(['MAC Address', 'Timer'])
+
+        # port 0 data
+        table.setItem(0, 0, QTableWidgetItem('00-B0-D0-63-C2-26'))
+        table.setItem(0, 1, QTableWidgetItem('-'))
+
+        # port 1 data
+        table.setItem(1, 0, QTableWidgetItem('00-B0-D0-63-C2-26'))
+        table.setItem(1, 1, QTableWidgetItem('-'))
+
+        table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        # Resize columns and rows to fit content
+        table.resizeColumnsToContents()
+        table.resizeRowsToContents()
+
+        # Create a QPlainTextEdit for text output
+        thread_id = threading.current_thread().ident
+        self.output_text.setPlaceholderText(f"Thread ID: {thread_id}")
+
+        print(f"Thread ID: {thread_id}")
+
+        # Create a central widget and set the table and output_text as its layout
+        central_widget = QWidget(self)
+        central_layout = QVBoxLayout(central_widget)
+        central_layout.addWidget(table)
+        central_layout.addWidget(self.output_text)
+
+        # Set stretch factor for the table in the layout
+        central_layout.setStretchFactor(table, 1)
+
+        # Set the central widget of the main window
+        self.setCentralWidget(central_widget)
+
+        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle('The Switcher')
+
+def main():
+    app = QApplication(sys.argv)
+    ex = TableExample()
+
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
