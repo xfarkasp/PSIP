9152e7da6c1dc21d96b93d65250d70d62c602e4d
xfarkasp <xfarkasp@stuba.sk>
2024-03-18 11:40:57 +0100

    new gui for mac table

--- gui_window.py
+++ gui_window.py
@@ -1,9 +1,11 @@
 import sys
 import threading
 
-from PyQt5.QtCore import QTimer, pyqtSlot, Qt
+from PyQt5.QtCore import QTimer, pyqtSlot, Qt, QSize
+from PyQt5.QtGui import QColor, QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, \
-    QPlainTextEdit, QSizePolicy, QLabel, QHBoxLayout, QHeaderView, QPushButton, QComboBox, QScrollBar
+    QPlainTextEdit, QSizePolicy, QLabel, QHBoxLayout, QHeaderView, QPushButton, QComboBox, QScrollBar, QListWidgetItem, \
+    QListWidget
 
 from Switch import Switch
 
@@ -14,7 +16,8 @@ class Ui(QMainWindow):
         self.output_text = QPlainTextEdit(self)
         self.timer_input_field = QPlainTextEdit(self)
         self.timer_update_button = QPushButton('Start', self)
-        self.mac_table = QTableWidget(self)
+        self.port1_widget = QListWidget()
+        self.port2_widget = QListWidget()
         self.stat_table = QTableWidget(self)
         # combo boxes for interface select
         self.port0_combo_box = QComboBox(self)
@@ -39,6 +42,39 @@ class Ui(QMainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.timer_callback)
         self.start_timer()
+
+    def create_port_label(self, text):
+        label = QLabel(text)
+        label.setAlignment(Qt.AlignCenter)
+        label.setFont(QFont("Arial", 14, QFont.Bold))
+        return label
+
+    def add_separator(self, list_widget):
+        separator_item = QListWidgetItem()
+        separator_item.setFlags(separator_item.flags() & ~Qt.ItemIsSelectable)  # Disable selection
+        separator_item.setSizeHint(QSize(1, 10))  # Set size
+        separator_item.setBackground(QColor(0, 0, 0))  # Set background color
+        list_widget.addItem(separator_item)
+
+    def populate_lists(self):
+        # Dummy data, you can replace this with your actual data
+        port_data = {
+            "Port 1": [("AA:BB:CC:DD:EE:FF", "00:00:20"), ("12:34:56:78:90:AB", "00:01:30")],
+            "Port 2": [("FF:EE:DD:CC:BB:AA", "00:02:45"), ("AB:90:78:56:34:12", "00:03:15")]
+        }
+
+        for port, macs in port_data.items():
+            if port == "Port 1":
+                list_widget = self.port1_widget
+            else:
+                list_widget = self.port2_widget
+
+            # Add column identifiers
+            list_widget.addItem("MAC\t\tTimer")
+            self.add_separator(list_widget)
+            for mac, timer in macs:
+                item = QListWidgetItem(f"{mac}\t{timer}")
+                list_widget.addItem(item)
 
     @pyqtSlot(str)
     def add_text(self, text):
@@ -96,27 +132,21 @@ class Ui(QMainWindow):
 
     def initUI(self):
         # WIDGETS
-        self.mac_table.setMaximumHeight(105)
-        self.mac_table.setMaximumWidth(235)
-        self.mac_table.setRowCount(2)
-        self.mac_table.setColumnCount(2)
+        layoutMac = QHBoxLayout()  # Horizontal layout
+        # Create a QVBoxLayout for each port
+        port1_layout = QVBoxLayout()
+        port2_layout = QVBoxLayout()
 
-        self.mac_table.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.mac_table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        port1_layout.addWidget(self.create_port_label("Port 1"))  # Add port name label
+        port1_layout.addWidget(self.port1_widget)
 
-        # MAC table
-        self.mac_table.setVerticalHeaderLabels(['wifi', 'PORT 1'])
-        self.mac_table.setHorizontalHeaderLabels(['MAC Address', 'Timer'])
-        self.mac_table.setColumnWidth(1, 30)
-        # mac_table.setFixedSize(205, 80)  # Set your preferred width and height
+        port2_layout.addWidget(self.create_port_label("Port 2"))  # Add port name label
+        port2_layout.addWidget(self.port2_widget)
 
-        # port 0 data
-        self.mac_table.setItem(0, 0, QTableWidgetItem('NONE'))
-        self.mac_table.setItem(0, 1, QTableWidgetItem('-'))
-
-        # port 1 data
-        self.mac_table.setItem(1, 0, QTableWidgetItem('NONE'))
-        self.mac_table.setItem(1, 1, QTableWidgetItem('-'))
+        # Add the QVBoxLayouts to the main QHBoxLayout
+        layoutMac.addLayout(port1_layout)
+        layoutMac.addLayout(port2_layout)
+        self.populate_lists()
 
         self.stat_table.setRowCount(10)
         self.stat_table.setColumnCount(4)
@@ -153,7 +183,7 @@ class Ui(QMainWindow):
         port_select_layout.addWidget(self.port1_combo_box)
         port_select_layout.addLayout(delay_update_layout)
 
-        mac_layout.addWidget(self.mac_table)
+        mac_layout.addLayout(layoutMac)
         mac_layout.addLayout(port_select_layout)
 
         table_layout.addLayout(mac_layout)
@@ -180,3 +210,87 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# import sys
+# from PyQt5.QtCore import Qt, QSize
+# from PyQt5.QtGui import QColor, QFont
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, QListWidget, QListWidgetItem, QLabel
+#
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("MAC Table")
+#
+#         self.central_widget = QWidget()
+#         self.setCentralWidget(self.central_widget)
+#
+#
+#         self.central_widget.setLayout(self.layoutMac)
+#
+#         self.layoutMac = QHBoxLayout()  # Horizontal layout
+#         # Create a QVBoxLayout for each port
+#         self.port1_layout = QVBoxLayout()
+#         self.port2_layout = QVBoxLayout()
+#
+#         self.port1_widget = QListWidget()
+#         self.port1_layout.addWidget(self.create_port_label("Port 1"))  # Add port name label
+#         self.port1_layout.addWidget(self.port1_widget)
+#
+#         self.port2_widget = QListWidget()
+#         self.port2_layout.addWidget(self.create_port_label("Port 2"))  # Add port name label
+#         self.port2_layout.addWidget(self.port2_widget)
+#
+#         # Add the QVBoxLayouts to the main QHBoxLayout
+#         self.layoutMac.addLayout(self.port1_layout)
+#         self.layoutMac.addLayout(self.port2_layout)
+#
+#         self.populate_lists()
+#
+#     def create_port_label(self, text):
+#         label = QLabel(text)
+#         label.setAlignment(Qt.AlignCenter)
+#         label.setFont(QFont("Arial", 14, QFont.Bold))
+#         return label
+#
+#     def add_separator(self, list_widget):
+#         separator_item = QListWidgetItem()
+#         separator_item.setFlags(separator_item.flags() & ~Qt.ItemIsSelectable)  # Disable selection
+#         separator_item.setSizeHint(QSize(1, 10))  # Set size
+#         separator_item.setBackground(QColor(0, 0, 0))  # Set background color
+#         list_widget.addItem(separator_item)
+#
+#     def populate_lists(self):
+#         # Dummy data, you can replace this with your actual data
+#         port_data = {
+#             "Port 1": [("AA:BB:CC:DD:EE:FF", "00:00:20"), ("12:34:56:78:90:AB", "00:01:30")],
+#             "Port 2": [("FF:EE:DD:CC:BB:AA", "00:02:45"), ("AB:90:78:56:34:12", "00:03:15")]
+#         }
+#
+#         for port, macs in port_data.items():
+#             if port == "Port 1":
+#                 list_widget = self.port1_widget
+#             else:
+#                 list_widget = self.port2_widget
+#
+#             # Add column identifiers
+#             list_widget.addItem("MAC\t\tTimer")
+#             self.add_separator(list_widget)
+#             for mac, timer in macs:
+#                 item = QListWidgetItem(f"{mac}\t{timer}")
+#                 list_widget.addItem(item)
+#
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.setGeometry(100, 100, 600, 300)
+#     window.show()
+#     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
